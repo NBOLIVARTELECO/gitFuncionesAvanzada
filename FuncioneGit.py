022047537565cca3254888d8ed28a4370587fafb
Nestor <nbolivar@unal.edu.co>
2025-04-28 11:11:42 -0500

    first commit

---
+++ FuncioneGit.py
@@ -0,0 +1,31 @@
+##Nombrar archivos
+#R1.  no caracartesres especiales
+
+# esto e s un comentario
+
+# primera variable de tipo String
+# Float
+# Int
+# booleano
+
+#declaracion de variable
+
+#Elsiguiente bloque recoje un dato por consola y lo imprimie
+#declaramos una funcion de x**2
+
+import math
+
+
+
+print("Verificacion del archivo")
+#area_circulo(10)
+
+
+
+
+#capturar un dato desde consola
+#type
+
+
+
+# esta es la nueva version del codigo
